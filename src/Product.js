062a2887aab86b60aa0e1f3f28f48d0413b87968
Dhruv Varshney <you@example.com>
2022-07-20 14:38:06 -0700

    fourth commit

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,40 @@
 import React from "react";
-import './Product'
+import './Product.css'
+import ProductOne from "./images/cereal.jfif"
 
-function Product(){
+function Product({title, Distance, Donor, rating, image}){
     return(
         <div className="product">
-
+            <div className="product__info">
+                <p>{title}</p>
+                
+                <p className="product__distance"> 
+                    <strong>{Distance}</strong>
+                    <small> Miles</small>
+                </p>
+                <div className="product__rating">
+                    <text>Donor Name: </text>
+                    <text>{Donor}</text>
+                </div>
+                <div className="product__rating">
+                    <text>Donor Rating: </text>
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                    <p>🌟</p>
+                    ))}
+                </div>
+                
+            </div>
+            <img src={image} alt=""/>
+            <button>Accept Item</button>
         </div>
+
+
+        
+
+      
     )
 }
+
+ export default Product
--- src/home.js
+++ src/home.js
@@ -2,18 +2,27 @@ import { requirePropFactory } from "@mui/material";
 import React from "react";
 import "./home.css";
 import HcImage from './images/homecover6.jpg'
+import Product from "./Product";
+import cereal from "./images/cereal.jfif"
 
 function Home(){
     return <div className="home">
         <div className="home__container">
             <img className="home__image" src={HcImage} alt=""/>
 
-            <div className="home__row">
-
+            <div className="home__row"> 
+                <Product title="Mini Wheat cereal"
+                Distance={2.5}
+                Donor="Mike Hammy"
+                rating={5}
+                image={cereal}/>
+                <Product/>
             </div>
 
             <div className="home__row">
-
+                <Product/>
+                <Product/>
+                <Product/>
             </div>
 
             <div className="home__row">
